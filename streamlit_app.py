4c44df2f0f45360fdbba77768203a15d60875809
Ajith48 <112891138+Ajith48@users.noreply.github.com>
2022-09-22 12:53:15 +0530

    Create streamlit_app.py
    Add the import statement
    Add the headings 
    Add the sub headings

---
+++ streamlit_app.py
@@ -0,0 +1,15 @@
+import streamlit 
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & bluebery Oatmeal')
+streamlit.text('🥗 Kale , Spianch & Rocket Smoothie')
+streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Tost')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
+import pandas 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
